192fb832af0df00410686fe3d2ba48826d4e4c4c
weitianming <qq806783737@live.com>
2016-04-15 15:07:58 +0800

    a10

--- src/com/example/push/PushAndroid.java
+++ src/com/example/push/PushAndroid.java
@@ -1,6 +1,9 @@
 package com.example.push;
 
+import com.aliyuncs.AcsRequest;
 import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
 import com.aliyuncs.push.model.v20150827.PushMessageToAndroidRequest;
 import com.aliyuncs.push.model.v20150827.PushMessageToAndroidResponse;
 import com.aliyuncs.push.model.v20150827.PushNoticeToAndroidRequest;
@@ -23,10 +26,10 @@ public class PushAndroid {
 	 * 向Android推送通知
 	 */
 	public void PushNoticeToAndroid(String sender, String receiver, String content) throws Exception {
+		IClientProfile clientProfile = DefaultProfile.getProfile("cn-hangzhou", "KzuqnYKPlkUZb4PS", "EH91BgTQYavkawQNlhTw1in8S2X9jm ");
+		DefaultAcsClient client = new DefaultAcsClient(clientProfile);
 		
 		
-		
-		DefaultAcsClient client = new DefaultAcsClient();
 		PushNoticeToAndroidRequest androidRequest = new PushNoticeToAndroidRequest();
         androidRequest.setAppKey(appKey);
         androidRequest.setTitle(sender); // 发送者
