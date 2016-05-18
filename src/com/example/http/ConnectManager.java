08ffd3129ff31d393af9e345f112b97a740b3977
weitianming <qq806783737@live.com>
2016-05-18 10:32:32 +0800

    移动推送1.6

--- src/com/example/http/ConnectManager.java
+++ src/com/example/http/ConnectManager.java
@@ -40,7 +40,7 @@ public class ConnectManager {
 	public String Notice(String sender, String receiver, String content) {
 
 		String DeviceId = new DataBaseDemo().QueryDeviceId(receiver);
-		DeviceId = "2270d39f46b548c983b88cd150100ba9";
+//		DeviceId = "2270d39f46b548c983b88cd150100ba9";
 		
 //		System.out.println("指定的ID号："+DeviceId);
 		
@@ -48,8 +48,8 @@ public class ConnectManager {
 		try {
 
 			if (new DeviceInfoDemoTest().testGetDeviceInfos(DeviceId)) { // 该ID在线发送消息
-//				new PushAndroid().PushMessageToAndroid(sender, DeviceId,
-//						content);
+				new PushAndroid().PushMessageToAndroid(sender, DeviceId,
+						content);
 				new PushAndroid()
 				.PushNoticeToAndroid(sender, DeviceId, content); // 离线发送推送
 			} else {
